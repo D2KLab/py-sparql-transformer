fe5fbccc6dcdeaf1b31f852d56576c3d179e4ef3
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2019-07-31 16:30:59 +0200

    mock tests

--- tests.py
+++ tests.py
@@ -1,50 +1,31 @@
-import unittest
-import json
-from simplejson import dumps
 import os
+import json
+import unittest
+from unittest.mock import patch
+from simplejson import dumps
 from SPARQLTransformer import sparqlTransformer
+import SPARQLTransformer
 
 OUTPUT = "./examples/json_transformed/"
 JSONLD_QUERIES = "./examples/json_queries/"
+SPARQL_OUTPUT = "./examples/sparql_output/"
 
 opt = {
     'debug': False
 }
 
 
-class TestStringMethods(unittest.TestCase):
+def mock(filename):
+    with open(os.path.join(SPARQL_OUTPUT, filename)) as data:
+        obj = json.load(data)
 
-    def test_proto(self):
-        q, expected = load('city.list.json')
-        out = sparqlTransformer(q, opt)
-        # with open('a.json', 'w') as o:
-        #     json.dump(out, o)
+    def f(self):
+        class x:
+            def convert():
+                return obj
+        return x
 
-        self.assertEqual(dumps(out), dumps(expected))
-
-    def test_jsonld(self):
-        q, expected = load('city.list.ld.json')
-        out = sparqlTransformer(q, opt)
-        # with open('a.json', 'w') as o:
-        #     json.dump(out, o)
-
-        self.assertEqual(dumps(out), dumps(expected))
-
-    def test_nested(self):
-        q, expected = load('city.region.list.ld.json')
-        out = sparqlTransformer(q, opt)
-        # with open('a.json', 'w') as o:
-        #     json.dump(out, o)
-
-        self.assertEqual(dumps(out), dumps(expected))
-
-    def test_anchor(self):
-        q, expected = load('band.json')
-        out = sparqlTransformer(q, opt)
-        # with open('a.json', 'w') as o:
-        #     json.dump(out, o)
-
-        self.assertEqual(dumps(out), dumps(expected))
+    return f
 
 
 def load(filename):
@@ -54,6 +35,45 @@ def load(filename):
         expected = json.load(data)
 
     return q, expected
+
+
+class TestStringMethods(unittest.TestCase):
+    @patch.object(SPARQLTransformer.SPARQLWrapper, 'query', mock('city.list.json'))
+    def test_proto(self):
+        q, expected = load('city.list.json')
+
+        out = sparqlTransformer(q, opt)
+        # with open('a.json', 'w') as o:
+        #     json.dump(out, o)
+
+        self.assertEqual(dumps(out), dumps(expected))
+
+    @patch.object(SPARQLTransformer.SPARQLWrapper, 'query', mock('city.list.ld.json'))
+    def test_jsonld(self):
+        q, expected = load('city.list.ld.json')
+        out = sparqlTransformer(q, opt)
+        # with open('a.json', 'w') as o:
+        #     json.dump(out, o)
+
+        self.assertEqual(dumps(out), dumps(expected))
+
+    @patch.object(SPARQLTransformer.SPARQLWrapper, 'query', mock('city.region.list.ld.json'))
+    def test_nested(self):
+        q, expected = load('city.region.list.ld.json')
+        out = sparqlTransformer(q, opt)
+        # with open('a.json', 'w') as o:
+        #     json.dump(out, o)
+
+        self.assertEqual(dumps(out), dumps(expected))
+
+    @patch.object(SPARQLTransformer.SPARQLWrapper, 'query', mock('band.json'))
+    def test_anchor(self):
+        q, expected = load('band.json')
+        out = sparqlTransformer(q, opt)
+        # with open('a.json', 'w') as o:
+        #     json.dump(out, o)
+
+        self.assertEqual(dumps(out), dumps(expected))
 
 
 if __name__ == '__main__':
